c08acaea8c44b84a81a93ee45ec18069abdf2720
ismail-elmoubaraki-portfolio <139160615+ismail-elmoubaraki-portfolio@users.noreply.github.com>
2023-07-11 18:47:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const generateMemeBtn = document.querySelector(  
+    ".meme-generator .memebtn"
+  );
+  const memeImage = document.querySelector(".meme-generator img");
+  const memeTitle = document.querySelector(".meme-generator .title");
+  
+  
+  const updateDetails = (url, title,) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    
+  };
+  
+  const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+      .then((response) => response.json())
+      .then((data) => {
+        updateDetails(data.url, data.title);
+      });
+  };
+  
+  memebtn.addEventListener("click", generateMeme);
+  
+  generateMeme();
